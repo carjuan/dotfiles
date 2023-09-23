2febd45d0c1c987a8d39e856991f76154c3a5605
Juanse <pcjvan@gmail.com>
2023-09-23 12:28:01 -0500

    feat(nvim): add and configure catppuccin colorscheme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -48,11 +48,57 @@ return {
         "EdenEast/nightfox.nvim",
     },
 
-    -- configure LazyVim to load gruvbox
+    -- add catppuccin
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        config = function()
+            require("catppuccin").setup({
+                flavour = "mocha", -- latte, frappe, macchiato, mocha
+                term_colors = true,
+                transparent_background = false,
+                no_italic = false,
+                no_bold = false,
+                styles = {
+                    comments = {},
+                    conditionals = {},
+                    loops = {},
+                    functions = {},
+                    keywords = {},
+                    strings = {},
+                    variables = {},
+                    numbers = {},
+                    booleans = {},
+                    properties = {},
+                    types = {},
+                },
+                color_overrides = {
+                    mocha = {
+                        base = "#000000",
+                        mantle = "#000000",
+                        crust = "#000000",
+                    },
+                },
+                highlight_overrides = {
+                    mocha = function(C)
+                        return {
+                            TabLineSel = { bg = C.pink },
+                            CmpBorder = { fg = C.surface2 },
+                            Pmenu = { bg = C.none },
+                            TelescopeBorder = { link = "FloatBorder" },
+                        }
+                    end,
+                },
+            })
+        end,
+    },
+
+    -- configure LazyVim to load colorscheme
     {
         "LazyVim/LazyVim",
         opts = {
-            colorscheme = "adwaita",
+            colorscheme = "catppuccin",
         },
     },
 }
