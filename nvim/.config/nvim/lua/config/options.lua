47cba8d7b72f30b98a5fab3a04d15aa6db60c1b4
Juanse <pcjvan@gmail.com>
2023-12-05 10:51:04 -0600

    feat(nvim): set indentation width to default as per lazyvim config

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -3,19 +3,16 @@
 -- Add any additional options here
 local opt = vim.opt
 
-opt.tabstop = 4 -- Number of spaces tabs count for
-opt.shiftwidth = 4 -- Size of indent
-opt.softtabstop = 4
 opt.errorbells = false
-opt.encoding = "utf-8"
+opt.encoding = 'utf-8'
 opt.swapfile = false
 opt.hlsearch = false
 opt.backup = false
 opt.scrolloff = 10
 opt.cmdheight = 0 -- Experimental feature
 opt.laststatus = 3
-opt.foldmethod = "marker"
-opt.colorcolumn = "90"
+opt.foldmethod = 'marker'
+opt.colorcolumn = '90'
 opt.conceallevel = 0 -- Shows text normally, specially for .json files ("") and .md lang
 
 -- More info on :h statusline
