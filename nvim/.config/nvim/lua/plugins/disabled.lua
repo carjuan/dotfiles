dcfbf5523341773534b3f109debe4209b89c4a40
Juanse <pcjvan@gmail.com>
2023-12-12 11:35:25 -0600

    feat(nvim): add plugins

--- nvim/.config/nvim/lua/plugins/disabled.lua
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -1,13 +1,10 @@
 return {
-    -- disable flash.nvim - <S> search
-    { "folke/flash.nvim", enabled = false },
+  -- disable flash.nvim - <S> search
+  { 'folke/flash.nvim', enabled = false },
 
-    -- disables notifications
-    { "rcarriga/nvim-notify", enabled = false },
+  -- disables vim.ui plugin for better input
+  { 'stevearc/dressing.nvim', enabled = false },
 
-    -- disables vim.ui plugin for better input
-    { "stevearc/dressing.nvim", enabled = false },
-
-    -- disables plugin that completely replaces the UI for messages, cmdline and the popupmenu
-    { "folke/noice.nvim", enabled = false },
+  -- disables plugin that completely replaces the UI for messages, cmdline and the popupmenu
+  -- { "folke/noice.nvim", enabled = false },
 }
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -2,10 +2,32 @@ return {
   {
     'nvim-treesitter/nvim-treesitter',
     opts = function(_, opts)
-      -- append 'scss' syntax highlighting
+      -- append 'scss'
       vim.list_extend(opts.ensure_installed, {
+        'astro',
+        'cmake',
+        'cpp',
+        'css',
+        'gitignore',
+        'graphql',
+        'http',
         'scss',
+        'sql',
+        'svelte',
       })
     end,
+
+    -- the following adds a new extension as a filetype .json
+    -- config = function(_, opts)
+    --   require('nvim-treesitter.configs').setup(opts)
+    --
+    --   -- MDX
+    --   vim.filetype.add({
+    --     extension = {
+    --       mdx = 'mdx',
+    --     },
+    --   })
+    --   vim.treesitter.language.register('markdown', 'mdx')
+    -- end,
   },
 }
