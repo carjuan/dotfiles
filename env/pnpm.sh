a4f768a3dcb736a923afd6ef1d70fb381fd587b2
Juanse Carvajal <pcjvan@gmail.com>
2024-03-17 14:42:54 -0500

    fix(env/pnpm.sh): make error out more verbose

--- env/pnpm.sh
+++ env/pnpm.sh
@@ -11,4 +11,5 @@ ensure_dependency "node" "pnpm"
 NODE_GLOBAL_PACKAGES="${DOTFILES}/dot_node_global_packages"
 
 [[ -s "${NODE_GLOBAL_PACKAGES}" ]] && xargs pnpm install -g <"${NODE_GLOBAL_PACKAGES}" ||
-	print_error "file ${NODE_GLOBAL_PACKAGES} not found"
+	print_error "either file ${NODE_GLOBAL_PACKAGES} was not found or pnpm could not run properly.
+    Check PNPM output for more INFO"
