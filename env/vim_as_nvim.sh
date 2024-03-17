3160ab5f556cbb5222cb0f2854d70f5e129ff723
Juanse Carvajal <pcjvan@gmail.com>
2024-03-17 14:45:45 -0500

    fix(env/vim_as_nvim.sh): make sure quiet_cd is called after directory is created

--- env/vim_as_nvim.sh
+++ env/vim_as_nvim.sh
@@ -10,9 +10,9 @@ notify "Setting up vim as nvim"
 
 USER_BIN="${HOME}/.local/bin"
 
-quiet_cd "${USER_BIN}"
-
 [[ ! -d ${USER_BIN} ]] && mkdir "${USER_BIN}"
+
+quiet_cd "${USER_BIN}"
 
 [[ ! -L ${USER_BIN}/vim ]] && ln -s "$(which nvim)" ./vim && echo "finished making ''vim' -> nvim" ||
 	echo "command 'vim' already -> nvim. No need to 'ln -s'"
