133ffe5c9a43ae108eb05e9677e0c44f97a619d9
Juan Carvajal <pcjvan@gmail.com>
2024-09-07 11:32:04 -0500

    nvim: remove intrusive hint from typescript

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -5,6 +5,20 @@ return {
     opts = {
       servers = {
         cssls = {},
+        vtsls = {
+          settings = {
+            typescript = {
+              inlayHints = {
+                enumMemberValues = { enabled = false },
+                functionLikeReturnTypes = { enabled = false },
+                parameterNames = { enabled = false },
+                parameterTypes = { enabled = false },
+                propertyDeclarationTypes = { enabled = false },
+                variableTypes = { enabled = false },
+              },
+            },
+          },
+        },
       },
     },
   },
