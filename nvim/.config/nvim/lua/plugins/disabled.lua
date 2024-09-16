086dbc627fea601221c77b2984940e778155c973
Juanse <pcjvan@gmail.com>
2024-09-16 08:31:01 -0500

    nvim: disable markdown highlight

--- nvim/.config/nvim/lua/plugins/disabled.lua
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -5,6 +5,9 @@ return {
   -- disables vim.ui plugin for better input
   { 'stevearc/dressing.nvim', enabled = false },
 
+  -- disables markdown highlight
+  { 'lukas-reineke/headlines.nvim', enabled = false },
+
   -- disables plugin that completely replaces the UI for messages, cmdline and the popupmenu
   -- { "folke/noice.nvim", enabled = false },
 }
