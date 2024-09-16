fde48da57737ca84e84a25722894d9e35b8ed0ff
Juanse <pcjvan@gmail.com>
2024-09-16 08:31:40 -0500

    nvim: update linters CL args

--- nvim/.config/nvim/lua/plugins/linting.lua
+++ nvim/.config/nvim/lua/plugins/linting.lua
@@ -4,8 +4,14 @@ return {
     linters_by_ft = {
       typescript = { 'eslint_d' },
       javascript = { 'eslint_d' },
-      bash = { 'shellcheck' },
-      sh = { 'shellcheck' },
+      bash = {
+        cmd = 'shellcheck',
+        args = { '-x' }, -- this option sets --external-source=true. Sourcing `./` is fine
+      },
+      sh = {
+        cmd = 'shellcheck',
+        args = { '-x' }, -- this option sets --external-source=true. Sourcing `./` is fine
+      },
       lua = { 'luacheck' },
     },
   },
