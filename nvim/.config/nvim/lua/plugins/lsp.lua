d722efc461a90a5b9112c56aa95641003477f6cc
Juanse <pcjvan@gmail.com>
2024-10-13 11:39:45 -0500

    nvim: add lsphover borders

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,3 +1,31 @@
+-- Adds borders to lsp hover tooltip
+vim.lsp.handlers['textDocument/hover'] = function(_, result, ctx, config)
+  config = config
+    or {
+      border = {
+        { '╭', 'Comment' },
+        { '─', 'Comment' },
+        { '╮', 'Comment' },
+        { '│', 'Comment' },
+        { '╯', 'Comment' },
+        { '─', 'Comment' },
+        { '╰', 'Comment' },
+        { '│', 'Comment' },
+      },
+    }
+  config.focus_id = ctx.method
+
+  local markdown_lines = vim.lsp.util.convert_input_to_markdown_lines(result.contents)
+
+  markdown_lines = vim.lsp.util.trim_empty_lines(markdown_lines)
+
+  if vim.tbl_isempty(markdown_lines) then
+    return
+  end
+
+  return vim.lsp.util.open_floating_preview(markdown_lines, 'markdown', config)
+end
+
 return {
   -- Sets up servers
   {
