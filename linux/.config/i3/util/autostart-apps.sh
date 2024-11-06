03a0c2c4ca842e38782ef2cb0c65c224e16d447d
Juanse <pcjvan@gmail.com>
2024-11-06 14:55:03 -0600

    terminal: update term to wezterm
    I wanted to render font ligatures and alacritty is not planning on
    implementing this anytime soon. Wezterm looked nice and also supported
    LUA to configure the terminal. I love LUA (thanks neovim) so I gave it a
    try.

--- linux/.config/i3/util/autostart-apps.sh
+++ linux/.config/i3/util/autostart-apps.sh
@@ -6,14 +6,14 @@ DP2_OUTPUT=$(xrandr | awk '/^DP-2 connected/{print $1}')
 DP4_OUTPUT=$(xrandr | awk '/^DP-4 connected/{print $1}')
 
 # open firefox in workspace 1 on hdmi-0
-i3-msg "workspace 1; move workspace to output $HDMI_OUTPUT; layout stacking; exec --no-startup-id alacritty"
+i3-msg "workspace 1; move workspace to output $HDMI_OUTPUT; layout stacking; exec --no-startup-id wezterm"
 i3-msg "workspace 1; exec --no-startup-id firefox https://chat.openai.com/"
 
 # Loop until the Firefox window with "chat" in its title is found and store its window ID in CHAT_OPEN_FIREFOX_ID
 # "Chat will be the name of the window when opened a brand new tab in chat.openai.com
 while [ -z "$CHAT_OPEN_FIREFOX_ID" ]; do
-	sleep 1
-	CHAT_OPEN_FIREFOX_ID=$(wmctrl -lx | grep -i "chat" | awk '{print $1}')
+  sleep 1
+  CHAT_OPEN_FIREFOX_ID=$(wmctrl -lx | grep -i "chat" | awk '{print $1}')
 done
 
 # Make the identified window floating, set its size to 700x2000, and position it at the top-right corner of the HDMI-0 monitor using i3-msg.
---
+++ linux/.config/wezterm/wezterm.lua
@@ -0,0 +1,64 @@
+local wezterm = require("wezterm")
+
+return {
+	-- Live reload
+	automatically_reload_config = true,
+
+	-- Colorscheme
+	colors = {
+		foreground = "#e6e6e6",
+		background = "#292a2b",
+		cursor_bg = "#ffb86c",
+		cursor_fg = "#fefff2",
+		cursor_border = "#ffb86c",
+		selection_bg = "#555555",
+		selection_fg = "#e6e6e6",
+
+		ansi = {
+			"#292a2b", -- black
+			"#ff2c6d", -- red
+			"#19f9d8", -- green
+			"#ffb86c", -- yellow
+			"#45a9f9", -- blue
+			"#ff75b5", -- magenta
+			"#6fc1ff", -- cyan
+			"#e6e6e6", -- white
+		},
+		brights = {
+			"#555555", -- bright black
+			"#ff2c6d", -- bright red
+			"#19f9d8", -- bright green
+			"#ffcc95", -- bright yellow
+			"#6fc1ff", -- bright blue
+			"#ff9ac1", -- bright magenta
+			"#6fc1ff", -- bright cyan
+			"#ffffff", -- bright white
+		},
+	},
+
+	-- Cursor settings
+	default_cursor_style = "BlinkingBlock",
+	cursor_blink_rate = 0, -- Blinking "Never"
+	force_reverse_video_cursor = false, -- equivalent to unfocused_hollow=false in Alacritty
+
+	-- Font settings
+	font = wezterm.font("FiraCode Nerd Font Propo", { weight = "Bold" }),
+	font_size = 16.0,
+	adjust_window_size_when_changing_font_size = false,
+
+	-- Font offset (not directly configurable in WezTerm, but this can be adjusted via padding)
+	window_padding = {
+		left = 1,
+		right = 0,
+		top = 0,
+		bottom = 0,
+	},
+
+	-- Clipboard settings
+	enable_csi_u_key_encoding = true, -- Similar behavior for copying text to clipboard
+
+	-- Window settings
+	window_decorations = "NONE",
+	enable_tab_bar = false,
+	--window_title = "Alacritty",
+}
