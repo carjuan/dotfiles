5166dce648aa140b7b99d5580bd75a7c8fbe43ee
Juan Carvajal <pcjvan@gmail.com>
2024-12-13 16:42:02 -0600

    nvim: remove duplicate eslint
    Lazyvim extra was already activated in
    '~/.config/nvim/lua/config/lazy.lua'

--- nvim/.config/nvim/lua/plugins/linting.lua
+++ nvim/.config/nvim/lua/plugins/linting.lua
@@ -2,8 +2,6 @@ return {
   'mfussenegger/nvim-lint',
   opts = {
     linters_by_ft = {
-      typescript = { 'eslint_d' },
-      javascript = { 'eslint_d' },
       bash = {
         cmd = 'shellcheck',
         args = { '-x' }, -- this option sets --external-source=true. Sourcing `./` is fine
