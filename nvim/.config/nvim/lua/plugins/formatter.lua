dac7a57cb53a2e938fbc7356523d4ad8733f8948
Juanse <pcjvan@gmail.com>
2025-07-05 22:12:08 -0500

    nvim: enable prettier on anything except javascript-like files

--- nvim/.config/nvim/lua/plugins/formatter.lua
+++ nvim/.config/nvim/lua/plugins/formatter.lua
@@ -1,28 +1,32 @@
+-- source: https://github.com/LazyVim/LazyVim/blob/25abbf546d564dc484cf903804661ba12de45507/lua/lazyvim/plugins/extras/formatting/prettier.lua
 local supported = {
-  'css',
-  'graphql',
-  'handlebars',
-  'html',
-  'json',
-  'jsonc',
-  'less',
-  'markdown',
-  'markdown.mdx',
-  'scss',
-  'vue',
-  'yaml',
+    'css',
+    'graphql',
+    'handlebars',
+    'html',
+    'json',
+    'jsonc',
+    'less',
+    'markdown',
+    'markdown.mdx',
+    'scss',
+    'vue',
+    'yaml',
+}
+
+local disabled = {
+    "javascript",
+    "javascriptreact",
+    "typescript",
+    "typescriptreact",
 }
 
 return {
-  'stevearc/conform.nvim',
-  -- FIX: with the help of lazyvim docs, and modifying formatters_by_ft
-  -- I can can have an if/else statment for javascript like files an disable
-  -- it for it.
-  opts = function(_, opts)
-    for _, ft in ipairs(supported) do
-      opts.formatters_by_ft = {
-        javascript = '',
-      }
-    end
-  end,
+    'stevearc/conform.nvim',
+    opts = function(_, opts)
+        opts.formatters_by_ft = opts.formatters_by_ft or {}
+        for _, ft in ipairs(disabled) do
+            opts.formatters_by_ft[ft] = {}
+        end
+    end,
 }
